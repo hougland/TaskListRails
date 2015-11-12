86a9325fad24d314e6fe3b9da4517a4cd50d4edb
Ricky Hougland <houglande@gmail.com>
2015-11-12 13:52:33 -0800

    finish w2, messing with new way of doing the delete button

--- taskslist/app/controllers/tasks_controller.rb
+++ taskslist/app/controllers/tasks_controller.rb
@@ -7,4 +7,23 @@ class TasksController < ApplicationController
     id = params[:id]
     @task = Task.find(id)
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params[:task])
+    redirect_to '/'
+  end
+
+  def destroy
+    Task.destroy(params["id"])
+    redirect_to '/'
+  end
+
+  private
+  def task_params
+    params.permit(task:[:name, :description])
+  end
 end
